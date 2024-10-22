59c91f60f48039eb262d08d386c62467963c3dea
Md. Zahidul Islam <mdzahidulislam.bera@gmail.com>
2024-10-22 12:34:01 +0600

    Create react and neccessary files, add and load fake data

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Blogs from './components/Blogs'
+
+function App() {
+
+
+  return (
+    <>
+
+      <h1 className='text-center text-4xl font-bold'>React Konwledge Cafe</h1>
+      <Blogs></Blogs>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Blogs.jsx
@@ -0,0 +1,21 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+
+const Blogs = () => {
+    const [blogs, setBlogs] = useState([]);
+
+    useEffect(() => {
+        fetch('blog.json')
+            .then(res => res.json())
+            .then(data => setBlogs(data))
+    }, [])
+
+    return (
+        <div>
+            <h2>Blogs: {blogs.length}</h2>
+        </div>
+    );
+};
+
+export default Blogs;
